8d0563fe4a3ab585802e7504d1b596894c1545f9
Sean Bradley <seanbradley@seans-macbook-pro.local>
2017-02-22 17:08:02 -0800

    Initial Commit

---
+++ bangfor_final/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bangfor_final
+//
+//  Created by Sean Bradley on 2/22/17.
+//  Copyright © 2017 Sean Bradley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
